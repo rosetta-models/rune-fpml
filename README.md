2ba3710cc90c863be6daffe7853f3a283457ec3e
PayalKhanna <payal.khanna@regnosys.com>
2024-06-11 11:33:13 +0100

    initial repo config changes

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# blank-drr-base-model
-a Blank repository template which could be used to create new model which are based on DRR Model
+# Fpml Model
--- rosetta-source/src/test/java/namespace/ValidateNamespaceTest.java
+++ rosetta-source/src/test/java/namespace/ValidateNamespaceTest.java
@@ -8,10 +8,12 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ValidateNamespaceTest {
    @Test
     void validateFileNamesMatchNamespace() throws IOException {
-        String modelShortName = "bank";
+        String modelShortName = "fpml";
         Path path = Paths.get("src/main/rosetta");
         RosettaFileNameValidator validator = new RosettaFileNameValidator(modelShortName, path, null);
 
