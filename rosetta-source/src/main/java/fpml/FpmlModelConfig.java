563d5c42fd0798df7fac6ba96336cc8701d7bf52
hugohills-regnosys <hugo.hills@regnosys.com>
2024-11-16 21:18:16 +0000

    Add other message types

--- rosetta-source/src/main/java/fpml/FpmlModelConfig.java
+++ rosetta-source/src/main/java/fpml/FpmlModelConfig.java
@@ -1,20 +1,32 @@
 package fpml;
 
 import com.google.common.collect.ImmutableMap;
-import fpml.confirmation.DataDocument;
+import fpml.confirmation.*;
 
 public class FpmlModelConfig {
 
     public static String SCHEMA_PATH = "schemas/fpml-5-13/confirmation/fpml-main-5-13.xsd";
     public static final String XML_CONFIG_PATH = "xml-config/confirmation-v5_13-rosetta-xml-config.json";
-    
-    public static final ImmutableMap<Class<?>, String> TYPE_TO_SCHEMA_MAP = ImmutableMap.of(
-           DataDocument.class, SCHEMA_PATH);
 
+    public static final ImmutableMap<Class<?>, String> TYPE_TO_SCHEMA_MAP =
+            ImmutableMap.<Class<?>, String>builder()
+                    .put(DataDocument.class, SCHEMA_PATH)
+                    .put(ClearingConfirmed.class, SCHEMA_PATH)
+                    .put(ExecutionAdvice.class, SCHEMA_PATH)
+                    .put(ExecutionAdviceRetracted.class, SCHEMA_PATH)
+                    .put(ExecutionNotification.class, SCHEMA_PATH)
+                    .put(RequestClearing.class, SCHEMA_PATH)
+                    .put(TradeChangeAdvice.class, SCHEMA_PATH)
+                    .build();
 
-    public static final ImmutableMap<Class<?>, String> TYPE_TO_XML_CONFIG_MAP = ImmutableMap.of(
-            DataDocument.class, XML_CONFIG_PATH);
-
-
-
+    public static final ImmutableMap<Class<?>, String> TYPE_TO_XML_CONFIG_MAP =
+            ImmutableMap.<Class<?>, String>builder()
+                    .put(DataDocument.class, XML_CONFIG_PATH)
+                    .put(ClearingConfirmed.class, XML_CONFIG_PATH)
+                    .put(ExecutionAdvice.class, XML_CONFIG_PATH)
+                    .put(ExecutionAdviceRetracted.class, XML_CONFIG_PATH)
+                    .put(ExecutionNotification.class, XML_CONFIG_PATH)
+                    .put(RequestClearing.class, XML_CONFIG_PATH)
+                    .put(TradeChangeAdvice.class, XML_CONFIG_PATH)
+                    .build();
 }
