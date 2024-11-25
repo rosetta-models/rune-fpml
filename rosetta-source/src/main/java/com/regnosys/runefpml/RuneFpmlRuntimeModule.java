9cb89a17057e7ee76256488807f6374bc06f5511
hugohills-regnosys <hugo.hills@regnosys.com>
2024-11-25 15:21:01 +0000

    Add runtime module

---
+++ rosetta-source/src/main/java/com/regnosys/runefpml/RuneFpmlRuntimeModule.java
@@ -0,0 +1,12 @@
+package com.regnosys.runefpml;
+
+import com.google.inject.AbstractModule;
+import com.rosetta.model.lib.ModuleConfig;
+
+@ModuleConfig(model = "RUNE-FPML", type = "Rosetta")
+public class RuneFpmlRuntimeModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        super.configure();
+    }
+}
