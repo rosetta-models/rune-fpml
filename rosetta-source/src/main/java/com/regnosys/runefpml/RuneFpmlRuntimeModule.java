770790a41dc3c0ed5cfa925664aa82fa1fdbe394
hugohills-regnosys <hugo.hills@regnosys.com>
2024-11-26 08:57:50 +0000

    Add runtime module bindings

--- rosetta-source/src/main/java/com/regnosys/runefpml/RuneFpmlRuntimeModule.java
+++ rosetta-source/src/main/java/com/regnosys/runefpml/RuneFpmlRuntimeModule.java
@@ -2,11 +2,24 @@ package com.regnosys.runefpml;
 
 import com.google.inject.AbstractModule;
 import com.rosetta.model.lib.ModuleConfig;
+import com.rosetta.model.lib.qualify.QualifyFunctionFactory;
+import com.rosetta.model.lib.validation.ValidatorFactory;
 
 @ModuleConfig(model = "RUNE-FPML", type = "Rosetta")
 public class RuneFpmlRuntimeModule extends AbstractModule {
+    
     @Override
     protected void configure() {
-        super.configure();
+        bind(QualifyFunctionFactory.class).to(bindQualifyFunctionFactory());
+        bind(ValidatorFactory.class).to(bindValidatorFactory());
     }
+
+    protected Class<? extends QualifyFunctionFactory> bindQualifyFunctionFactory() {
+        return QualifyFunctionFactory.Default.class;
+    }
+
+    protected Class<? extends ValidatorFactory> bindValidatorFactory() {
+        return ValidatorFactory.Default.class;
+    }
+
 }
