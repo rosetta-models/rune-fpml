dce337f06b04cdfe24a04fa3ebeaec645fa798d0
hugohills-regnosys <hugo.hills@regnosys.com>
2025-01-21 16:19:05 +0000

    Add RateChoice type and func

--- rosetta-source/src/main/java/com/regnosys/runefpml/RuneFpmlRuntimeModule.java
+++ rosetta-source/src/main/java/com/regnosys/runefpml/RuneFpmlRuntimeModule.java
@@ -6,16 +6,19 @@ import com.rosetta.model.lib.qualify.QualifyFunctionFactory;
 import com.rosetta.model.lib.validation.ValidatorFactory;
 import fpml.confirmation.custom.functions.ToProductChoice;
 import fpml.confirmation.custom.functions.ToProductChoiceImpl;
+import fpml.confirmation.custom.functions.ToRateChoice;
+import fpml.confirmation.custom.functions.ToRateChoiceImpl;
 
 @ModuleConfig(model = "RUNE-FPML", type = "Rosetta")
 public class RuneFpmlRuntimeModule extends AbstractModule {
-    
+
     @Override
     protected void configure() {
         bind(QualifyFunctionFactory.class).to(bindQualifyFunctionFactory());
         bind(ValidatorFactory.class).to(bindValidatorFactory());
-        
+
         bind(ToProductChoice.class).to(ToProductChoiceImpl.class);
+        bind(ToRateChoice.class).to(ToRateChoiceImpl.class);
     }
 
     protected Class<? extends QualifyFunctionFactory> bindQualifyFunctionFactory() {
@@ -25,5 +28,4 @@ public class RuneFpmlRuntimeModule extends AbstractModule {
     protected Class<? extends ValidatorFactory> bindValidatorFactory() {
         return ValidatorFactory.Default.class;
     }
-
 }
---
+++ rosetta-source/src/main/java/fpml/confirmation/custom/functions/ToRateChoiceImpl.java
@@ -0,0 +1,24 @@
+package fpml.confirmation.custom.functions;
+
+import fpml.confirmation.*;
+import fpml.confirmation.custom.RateChoice;
+
+public class ToRateChoiceImpl extends ToRateChoice {
+
+    @Override
+    protected RateChoice.RateChoiceBuilder doEvaluate(Rate rate) {
+        RateChoice.RateChoiceBuilder builder = RateChoice.builder();
+        if (rate instanceof RateCalculation) {
+            return builder.setRateCalculation(((RateCalculation) rate));
+        } else if (rate instanceof InflationRateCalculation) {
+            return builder.setInflationRateCalculation(((InflationRateCalculation) rate));
+        } else if (rate instanceof LoanFloatingRate) {
+            return builder.setLoanFloatingRate(((LoanFloatingRate) rate));
+        } else if (rate instanceof FloatingRateCalculation) {
+            return builder.setFloatingRateCalculation(((FloatingRateCalculation) rate));
+        } else if (rate instanceof StubFloatingRate) {
+            return builder.setStubFloatingRate(((StubFloatingRate) rate));
+        }
+        return builder;
+    }
+}
