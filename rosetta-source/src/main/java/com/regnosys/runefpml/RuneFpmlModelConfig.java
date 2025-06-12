85acd7ebc11d8a7ab628734eb46ae939c1a63730
Hugo Hills <hugo.hills@regnosys.com>
2025-06-12 11:16:03 +0100

    Fix schema and xml config map

--- rosetta-source/src/main/java/com/regnosys/runefpml/RuneFpmlModelConfig.java
+++ rosetta-source/src/main/java/com/regnosys/runefpml/RuneFpmlModelConfig.java
@@ -11,24 +11,10 @@ public class RuneFpmlModelConfig {
     public static final ImmutableMap<Class<?>, String> TYPE_TO_SCHEMA_MAP =
             ImmutableMap.<Class<?>, String>builder()
                     .put(Document.class, SCHEMA_PATH)
-                    .put(DataDocument.class, SCHEMA_PATH)
-                    .put(ClearingConfirmed.class, SCHEMA_PATH)
-                    .put(ExecutionAdvice.class, SCHEMA_PATH)
-                    .put(ExecutionAdviceRetracted.class, SCHEMA_PATH)
-                    .put(ExecutionNotification.class, SCHEMA_PATH)
-                    .put(RequestClearing.class, SCHEMA_PATH)
-                    .put(TradeChangeAdvice.class, SCHEMA_PATH)
                     .build();
 
     public static final ImmutableMap<Class<?>, String> TYPE_TO_XML_CONFIG_MAP =
             ImmutableMap.<Class<?>, String>builder()
-                    .put(Document.class, SCHEMA_PATH)
-                    .put(DataDocument.class, XML_CONFIG_PATH)
-                    .put(ClearingConfirmed.class, XML_CONFIG_PATH)
-                    .put(ExecutionAdvice.class, XML_CONFIG_PATH)
-                    .put(ExecutionAdviceRetracted.class, XML_CONFIG_PATH)
-                    .put(ExecutionNotification.class, XML_CONFIG_PATH)
-                    .put(RequestClearing.class, XML_CONFIG_PATH)
-                    .put(TradeChangeAdvice.class, XML_CONFIG_PATH)
+                    .put(Document.class, XML_CONFIG_PATH)
                     .build();
 }
