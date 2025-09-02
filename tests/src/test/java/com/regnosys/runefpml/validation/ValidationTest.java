6307fdb3075609b8da15d3d2db9058dd67958990
Hugo Hills <hugo.hills@regnosys.com>
2025-09-02 11:00:29 +0100

    Add validation test

---
+++ tests/src/test/java/com/regnosys/runefpml/validation/ValidationTest.java
@@ -0,0 +1,37 @@
+package com.regnosys.runefpml.validation;
+
+import com.google.inject.Guice;
+import com.regnosys.rosetta.common.validation.RosettaTypeValidator;
+import com.regnosys.rosetta.common.validation.ValidationReport;
+import com.regnosys.runefpml.RuneFpmlTestRuntimeModule;
+import fpml.confirmation.DataDocument;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
+public class ValidationTest {
+
+    @Inject
+    RosettaTypeValidator validateProcessor;
+
+    @BeforeEach
+    protected void createInjectorAndInject() {
+        Guice.createInjector(new RuneFpmlTestRuntimeModule()).injectMembers(this);
+    }
+    
+    @Test
+    public void shouldValidateSample() throws RuntimeException {
+        // Deserialise (and resolve references)
+        String resourceName = "serialisation/input/fpml-5-10/products/credit/cd-ex01-long-asia-corp-fixreg-versioned.xml";
+        DataDocument dataDocument = ResourcesUtils.getObjectAndResolveReferences(DataDocument.class, resourceName);
+        DataDocument.DataDocumentBuilder dataDocumentBuilder = dataDocument.toBuilder();
+
+        // Validate
+        ValidationReport validationReport = validateProcessor.runProcessStep(DataDocument.class, dataDocumentBuilder);
+        validationReport.logReport(); // logs validation failures as DEBUG and validation success as TRACE
+        assertThat(validationReport.validationFailures(), hasSize(12));
+    }
+}
