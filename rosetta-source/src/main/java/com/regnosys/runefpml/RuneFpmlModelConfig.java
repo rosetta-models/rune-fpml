9942810064d216f6bb0ade00028707814c82b0aa
Hugo Hills <39260692+hugohills-regnosys@users.noreply.github.com>
2025-10-17 12:24:06 +0100

    Update RuneFpmlModelConfig.java

--- rosetta-source/src/main/java/com/regnosys/runefpml/RuneFpmlModelConfig.java
+++ rosetta-source/src/main/java/com/regnosys/runefpml/RuneFpmlModelConfig.java
@@ -6,7 +6,7 @@ import fpml.consolidated.doc.Document;
 public class RuneFpmlModelConfig {
 
     public static String SCHEMA_PATH = "schemas/fpml-5-13/confirmation/fpml-main-5-13.xsd";
-    public static final String XML_CONFIG_PATH = "xml-config/confirmation-v5_13-rosetta-xml-config.json";
+    public static final String XML_CONFIG_PATH = "xml-config/fpml-5-13-confirmation-xml-config.json";
 
     public static final ImmutableMap<Class<?>, String> TYPE_TO_SCHEMA_MAP =
             ImmutableMap.<Class<?>, String>builder()
